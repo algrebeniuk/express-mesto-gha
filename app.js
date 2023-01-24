48822759aef63a0808368f52ffc0ac959163d763
Alexander Grebeniuk <al.al.grebeniuk@gmail.com>
2023-01-25 01:09:32 +0300

    added helmet

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 100, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -25,7 +25,7 @@ connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(json());
 app.use(limiter);
-app.use(helmet);
+app.use(helmet());
 
 app.post('/signup', validationOfUserSignUp, createUser);
 app.post('/signin', validationOfUserSignIn, login);
